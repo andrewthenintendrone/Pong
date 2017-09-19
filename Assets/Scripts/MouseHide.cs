5919c497f8356e3ac0954b2ff26fa49a00edb693
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-09-19 10:54:46 +1000

    Hide mouse on windows and reinclude builds by renaming build folder to bin

---
+++ Assets/Scripts/MouseHide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHide : MonoBehaviour
+{
+    // hides the mouse on windows
+	void Start ()
+    {
+#if UNITY_STANDALONE_WIN
+        Cursor.visible = false;
+#endif
+    }
+}
